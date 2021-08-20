c615bb4492b1e5db29e2e13ad39457bbfb01b967
Rupa Mainali <rupa.mainali16@gmail.com>
2021-08-20 18:43:21 -0400

    first commit

---
+++ Controllers/PayRuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PayRuRemit.Controllers
+{
+    public class PayRuController : Controller
+    {
+        // GET: PayRu
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PayRuRemit.MvcApplication" Language="C#" %>
